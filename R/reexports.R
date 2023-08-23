cc1df7b940c371930baf11a10a93c420d4b8c75e
jslit <jslit@jungsoo080ui-MacBookPro.local>
2023-08-23 22:32:13 +0900

    fix

--- R/reexports.R
+++ R/reexports.R
@@ -1,10 +1,2 @@
 # one_to_ten_just_for_test <- c(1:10)
 # usethis::use_data(one_to_ten_just_for_test)
-
-#' @importFrom dplyr %>%
-#' @export
-dplyr::`%>%`
-
-#' @importFrom readr read_fwf
-#' @export
-readr::read_fwf

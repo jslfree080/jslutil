c9295c6111397a567e58baea1006173784e81d5a
jslit <jslit@jungsoo080ui-MacBookPro.local>
2023-08-23 19:37:55 +0900

    remove param input txtdir with devtools::document()

--- R/plotreadtxtutil.R
+++ R/plotreadtxtutil.R
@@ -5,13 +5,12 @@
 #' Any rows with duplicated row names will be dropped with the first one being
 #' kepted.
 #'
-#' @param txtdir path to the input text file
 #' @param rdsdir path to the output rds file
 #' @return saves rds object for plotreading test
 #' @export
-saveRDSforplotread <- function(txtdir, rdsdir, os = "macOS"){
-  plotread_txt <- setwd(txtdir) %>% Sys.glob("*.txt")
-  ess_plotread_validation_input <- purrr::map(seq_along(plotread_txt), ~ read_fwf(plotread_txt[.], col_positions = fwf_widths(rep(1, 90))) %>% .[,which(.[3,] == "*")])
+saveRDSforplotread <- function(rdsdir, os = "macOS"){
+  plotread_txt <- Sys.glob("*.txt")
+  ess_plotread_validation_input <- map(seq_along(plotread_txt), ~ read_fwf(plotread_txt[.], col_positions = fwf_widths(rep(1, 90))) %>% .[,which(.[3,] == "*")])
   osmark <- ifelse(os == "macOS", "/", "\\")
   saveRDS(ess_plotread_validation_input, file = file.path(rdsdir, "ess_plotread_validation_input.rds", fsep = osmark))
 }

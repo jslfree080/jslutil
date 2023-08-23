4f654e17e1818e84c8f8038697226c3dabe1a8d8
jslit <jslit@jungsoo080ui-MacBookPro.local>
2023-08-23 22:27:32 +0900

    dplyr::

--- R/reexports.R
+++ R/reexports.R
@@ -1,9 +1,9 @@
 # one_to_ten_just_for_test <- c(1:10)
 # usethis::use_data(one_to_ten_just_for_test)
 
-#' @importFrom magrittr %>%
+#' @importFrom dplyr %>%
 #' @export
-magrittr::`%>%`
+dplyr::`%>%`
 
 #' @importFrom readr %>%
 #' @export
